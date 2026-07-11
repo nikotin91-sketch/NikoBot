ad7540c5ca967fe17955566f6af266c4276a9092
nikotin91-sketch <nikotin91@windowslive.com>
2026-07-11 18:32:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,73 +1,46 @@
 import os
 
 
-# ======================
-# API AYARLARI
-# ======================
-
-BTCTURK_API_KEY = os.getenv(
-    "BTCTURK_API_KEY",
-    ""
-)
-
-BTCTURK_SECRET = os.getenv(
-    "BTCTURK_SECRET",
-    ""
+# Tarama aralığı (saniye)
+SCAN_INTERVAL = int(
+    os.getenv(
+        "SCAN_INTERVAL",
+        60
+    )
 )
 
 
+# Aynı coin aynı sinyal tekrar süresi
+# 300 saniye = 5 dakika
+SIGNAL_COOLDOWN = int(
+    os.getenv(
+        "SIGNAL_COOLDOWN",
+        300
+    )
+)
 
-# ======================
-# TELEGRAM
-# ======================
+
+
+# Telegram ayarları
 
 TELEGRAM_TOKEN = os.getenv(
-    "TELEGRAM_TOKEN",
-    ""
+    "TELEGRAM_TOKEN"
 )
+
 
 TELEGRAM_CHAT_ID = os.getenv(
-    "TELEGRAM_CHAT_ID",
-    ""
+    "TELEGRAM_CHAT_ID"
 )
 
 
 
-# ======================
-# ÇALIŞMA MODU
-# ======================
+# BtcTurk API
 
-# signal = sadece alarm
-# paper = sanal işlem
-# live = gerçek işlem
-
-TRADING_MODE = os.getenv(
-    "TRADING_MODE",
-    "signal"
+BTCTURK_API_KEY = os.getenv(
+    "BTCTURK_API_KEY"
 )
 
 
-
-# ======================
-# AI AYARLARI
-# ======================
-
-BUY_SCORE = 85
-
-WATCH_SCORE = 70
-
-
-
-# ======================
-# RİSK AYARLARI
-# ======================
-
-MAX_POSITION_PERCENT = 10
-
-
-
-# ======================
-# TARAMA
-# ======================
-
-SCAN_INTERVAL = 60
+BTCTURK_SECRET = os.getenv(
+    "BTCTURK_SECRET"
+)
